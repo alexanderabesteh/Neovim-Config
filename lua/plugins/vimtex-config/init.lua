4d91a4ccd77b16d785a0aee908b5cb9f721cb024
alexanderabesteh <alexander.abesteh@protonmail.com>
2026-01-01 16:02:44 -0500

    Remove unnecessary code and add zathura

--- lua/plugins/vimtex-config/init.lua
+++ lua/plugins/vimtex-config/init.lua
@@ -1,14 +1,1 @@
---> Vimtex <--
-local M = {}
-
-function M.getCompletionItems(prefix)
-    -- define your total completion items
-    local items = vim.api.nvim_call_function('vimtex#complete#omnifunc', { 0, prefix })
-    return items
-end
-
-M.complete_item = {
-    item = M.getCompletionItems
-}
-
-return M
+vim.g.vimtex_view_method = "zathura"
